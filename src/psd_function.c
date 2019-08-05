ff0d58e5999ca2756201ffe799be41332daf0753
mturbin <mturbin@elvees.com>
2019-08-05 13:00:57 +0300

    up

--- src/psd_function.c
+++
@@ -1,9 +0,0 @@
-#include <math.h>
-#include <stdio.h>
-#define DELTA 1
-
-void psd_function(double *x, double *y, int n){
-    int i=0;
-    for(i=0;i<n;i++, x++, y++)
-        *y=1./(1.+pow(M_PI*(*x),2));
-}
